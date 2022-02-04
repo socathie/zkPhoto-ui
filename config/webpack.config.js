b9413c767d1765477f9f25b7bcbbdf041fb73d47
Cathie So <socathie@gmail.com>
2022-02-05 04:48:50 +0800

    browserify

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -298,6 +298,14 @@ module.exports = function (webpackEnv) {
       modules: ['node_modules', paths.appNodeModules].concat(
         modules.additionalModulePaths || []
       ),
+      fallback: {
+        "fs": false,
+        "crypto": require.resolve("crypto-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "stream": require.resolve("stream-browserify"),
+        "path": require.resolve("path-browserify"),
+        "worker_threads": false
+      },
       // These are the reasonable defaults supported by the Node ecosystem.
       // We also include JSX as a common component filename extension to support
       // some tools, although we do not recommend using it, see:
