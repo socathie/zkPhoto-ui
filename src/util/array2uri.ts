5d22136ca0b1247747b88a2db440a83f66fbb260
Cathie So <socathie@gmail.com>
2022-04-16 14:46:57 +0800

    Fixed #1 by eliminating usage of canvas or context

--- src/util/array2uri.ts
+++ src/util/array2uri.ts
@@ -4,56 +4,44 @@ import { Buffer } from "buffer";
 
 export default async function array2uri(array: Array<Array<any>>) {
 
-    const canvas = document.createElement('canvas');
-    canvas.width = 64;
-    canvas.height = 64;
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    let imgArray = Array.from({ length: 64 }, e => Array.from({ length: 64 }, e => Array.from({ length: 4 }, e => 255)));
 
     for (let k = 0; k < 16; k++) {
         let x = (k % 4) * 16;
         let y = (k / 4 >> 0) * 16;
         let idx = 0;
-        let sliceArray = Array.from({ length: 16 }, e => Array.from({ length: 16 }, e => Array.from({ length: 4 }, e => 255)));
-        //console.log(sliceArray);
 
         for (let j = 0; j < 16; j += 2) {
             for (let i = 0; i < 16; i += 2) {
                 let tmp = BigNumber.from(array[k][idx]).toHexString().slice(2).padStart(30, '0');
                 //console.log(tmp);
 
-                sliceArray[j][i][0] = parseInt(tmp.slice(0, 2), 16);
-                sliceArray[j][i][1] = parseInt(tmp.slice(2, 4), 16);
-                sliceArray[j][i][2] = parseInt(tmp.slice(4, 6), 16);
-                sliceArray[j][i][3] = 255;
+                imgArray[y + j][x + i][0] = parseInt(tmp.slice(0, 2), 16);
+                imgArray[y + j][x + i][1] = parseInt(tmp.slice(2, 4), 16);
+                imgArray[y + j][x + i][2] = parseInt(tmp.slice(4, 6), 16);
 
-                sliceArray[j][i + 1][0] = parseInt(tmp.slice(8, 10), 16);
-                sliceArray[j][i + 1][1] = parseInt(tmp.slice(10, 12), 16);
-                sliceArray[j][i + 1][2] = parseInt(tmp.slice(12, 14), 16);
-                sliceArray[j][i + 1][3] = 255;
+                imgArray[y + j][x + i + 1][0] = parseInt(tmp.slice(8, 10), 16);
+                imgArray[y + j][x + i + 1][1] = parseInt(tmp.slice(10, 12), 16);
+                imgArray[y + j][x + i + 1][2] = parseInt(tmp.slice(12, 14), 16);
 
-                sliceArray[j + 1][i][0] = parseInt(tmp.slice(16, 18), 16);
-                sliceArray[j + 1][i][1] = parseInt(tmp.slice(18, 20), 16);
-                sliceArray[j + 1][i][2] = parseInt(tmp.slice(20, 22), 16);
-                sliceArray[j + 1][i][3] = 255;
+                imgArray[y + j + 1][x + i][0] = parseInt(tmp.slice(16, 18), 16);
+                imgArray[y + j + 1][x + i][1] = parseInt(tmp.slice(18, 20), 16);
+                imgArray[y + j + 1][x + i][2] = parseInt(tmp.slice(20, 22), 16);
 
-                sliceArray[j + 1][i + 1][0] = parseInt(tmp.slice(24, 26), 16);
-                sliceArray[j + 1][i + 1][1] = parseInt(tmp.slice(26, 28), 16);
-                sliceArray[j + 1][i + 1][2] = parseInt(tmp.slice(28, 30), 16);
-                sliceArray[j + 1][i + 1][3] = 255;
+                imgArray[y + j + 1][x + i + 1][0] = parseInt(tmp.slice(24, 26), 16);
+                imgArray[y + j + 1][x + i + 1][1] = parseInt(tmp.slice(26, 28), 16);
+                imgArray[y + j + 1][x + i + 1][2] = parseInt(tmp.slice(28, 30), 16);
 
                 idx++;
             }
         }
-        let imageData = new ImageData(Uint8ClampedArray.from(sliceArray.flat().flat()), 16, 16);
-
-        ctx.putImageData(imageData, x, y);
     }
 
     //console.log(ctx.getImageData(0,0,64,64));
 
     const jimp = new Jimp(64, 64) as Jimp;
     
-    jimp.bitmap.data = Buffer.from(ctx.getImageData(0,0,64,64).data.buffer);
+    jimp.bitmap.data = Buffer.from(imgArray.flat().flat());
 
     //console.log(jimp);
 
